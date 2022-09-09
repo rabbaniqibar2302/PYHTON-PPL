1eb2e690d1a0c2956ad8c5a0b0eab71edbbfadb7
rizkiakbar2302@gmail.com <rizkiakbar2302@gmail.com>
2022-09-09 13:41:37 +0700

    complete all 2

--- RPL 1/latihanJS/script.js
+++ RPL 1/latihanJS/script.js
@@ -5,5 +5,5 @@ var characters = document.querySelector('.characters')
 text.addEventListener('input', () => {
     characters.textContent = text.value.length
     var txt = text.value.trim()
-    words.textContent = text.split(/\s+/).filter((item) => item).length
+    words.textContent = txt.split(/\s+/).filter((item) => item).length
 })
