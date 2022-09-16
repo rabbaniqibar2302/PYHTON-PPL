ff07842c1e28081e0360538e1cf371cc87adf406
rizkiakbar2302@gmail.com <rizkiakbar2302@gmail.com>
2022-09-16 09:27:04 +0700

    add projek rpl 1 yaa

---
+++ RPL 1/projek2ppl.py
@@ -0,0 +1,39 @@
+# Perpus kosong untuk menyimpan buku
+buku = []
+
+#fungsi show buku ( perlihatkan buku )
+def show_buku():
+
+
+#fungsi insert buku
+def insert_buku():
+    buku_baru = input("Judul Buku : ")
+    buku.append(buku_baru)
+
+# Menu untuk tampilan perpus
+def show_menu():
+    print("-Selamat datang di Perpus-")
+    print("1. Show buku")
+    print("2. Insert buku")
+    print("3. Edit buku")
+    print("4. Delete buku")
+    print("5. Keluar")
+
+    menu = int(input("Pilih Menu : > "))
+
+    if menu == 1:
+        show_buku()
+    elif menu == 2:
+        insert_buku()
+    elif menu == 3:
+        edit_buku()
+    elif menu == 4:
+        delete_buku()
+    elif menu == 5:
+        exit()
+    else:
+        print ("Upss Salaahh")
+
+# tampilkan Menu
+show_menu()
+print (buku)
