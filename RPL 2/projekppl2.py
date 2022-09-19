af83eeb571c4bbb31972b60d062802f9d80309d1
rizkiakbar2302@gmail.com <rizkiakbar2302@gmail.com>
2022-09-19 10:00:17 +0700

    add fungsi delete

--- RPL 2/projekppl2.py
+++ RPL 2/projekppl2.py
@@ -1,7 +1,9 @@
-#chest penyimpanan buku
+# chest penyimpanan buku
 buku = []
 
-#fungsi untuk menampilkan menu
+# fungsi untuk menampilkan menu
+
+
 def show_menu():
     print("---MENU---")
     print("1. Show Buku")
@@ -12,7 +14,6 @@ def show_menu():
 
     menu = int(input("Pilih Menu : "))
     print("\n")
-
     if menu == 1:
         show_buku()
     elif menu == 2:
@@ -26,18 +27,34 @@ def show_menu():
     else:
         print("Ente salah pilih")
 
-#fungsi untuk meng-insert buku
+# fungsi untuk meng-insert buku
+
+
 def insert_buku():
     buku_baru = input("Masukan Judul : ")
     buku.append(buku_baru)
 
-#fungsi untuk menampilkan isi buku
+# fungsi untuk menampilkan isi buku
+
+
 def show_buku():
     if len(buku) <= 0:
-        print ("Belum Ada Data")
+        print("Belum Ada Data")
     else:
         for indeks in range(len(buku)):
-            print ("[{}] {}".format(indeks,buku[indeks]))
+            print("[{}] {}".format(indeks, buku[indeks]))
+
+# fungsi untuk menghapus bukunya
+
+
+def delete_buku():
+    show_buku()
+    indeks = int(input("inputkan ID Buku : "))
+    if indeks > len(buku):
+        print("ID Salah")
+    else:
+        buku.remove(buku[indeks])
+
 
 if __name__ == "__main__":
     while True:
