c3eff1d8b6627e2a3da072504dd5ecff2d952160
rizkiakbar2302@gmail.com <rizkiakbar2302@gmail.com>
2022-09-19 08:49:26 +0700

    add projek 2 rpl 2

--- RPL 2/projekppl2.py
+++ RPL 2/projekppl2.py
@@ -1,74 +1,40 @@
-# input data
-stock = {}
-# dengan struktur {id : 'jenis_barang','merk_barang','qty'}
+#chest penyimpanan buku
+buku = []
 
-# Home
+#fungsi untuk menampilkan menu
+def show_menu():
+    print("---MENU---")
+    print("1. Show Buku")
+    print("2. Insert Buku")
+    print("3. Edit Buku")
+    print("4. Delete Buku")
+    print("5. Exit")
 
+    menu = int(input("Pilih Menu : "))
+    print("\n")
 
-def garis():
-    print('='*29)
+    if menu == 1:
+        show_buku()
+    elif menu == 2:
+        insert_buku()
+    elif menu == 3:
+        edit_buku()
+    elif menu == 4:
+        delete_buku()
+    elif menu == 5:
+        exit()
+    else:
+        print("Ente salah pilih")
 
+#fungsi untuk meng-insert buku
+def insert_buku():
+    buku_baru = input("Masukan Judul : ")
+    buku.append(buku_baru)
 
-def menu_awal():
-    garis()
-    print("    CRUD SYSTEM    ")
-    garis()
-    print("Selamat Datang di aplikasi CRUD")
-    print("Gudang kami bisa menyimpan barang")
-    print("Sesuai Kebutuhan Anda!")
-    garis()
+#fungsi untuk menampilkan isi buku
+def show_buku():
+    print(buku)
 
-
-def menu():
-    garis()
-    print("=Storage Management Menu=")
-    garis()
-    print("(1) - Input Barang")
-    print("(2) - Ambil Barang")
-    print("(3) - Cek Barang")
-    print("(4) - End Program")
-    return int(input("= "))
-
-
-run = menu_awal()
-run = menu()
-
-# Data
-
-
-class sistem:  # kelas sistem
+if __name__ == "__main__":
     while True:
-        if run == 1:
-            auth1 = int(input("Barang Baru : 1 atau Barang Lama : 2 ?"))
-            if auth1 == 1:
-                id_barang = int(input("Masukan Id Barang = "))
-                jenis_barang = input("Masukan Jenis Barang = ")
-                merk_barang = input("Masukan Merk Barang = ")
-                qty_barang = int(input("Masukan Qty Barang = "))
-                garis()
-                stock[id_barang] = jenis_barang, merk_barang, qty_barang
-                print(stock[id_barang])
-                print("Barang Berhasil Di Tambah")
-                choice = int(
-                    input("ketik 4 untuk lanjut atau ketik 5 untuk keluar"))
-                if choice == 4:
-                    run = menu()
-                else:
-                    break
-            elif run == 2:
-                print("Fitur masih dalam pengembangan")
-        elif run == 3:
-            print("BARANG YANG TERSEDIA")
-            print("-Id Barang--Jenis--Merk--QTY--")
-            for key, value in stock.items():
-                print("{}:{}".format(key, value))
-            garis()
-            choice = int(
-                input("ketik 4 untuk lanjut atau ketik 5 untuk keluar"))
-            if choice == 4:
-                run = menu()
-            else:
-                break
-# Tambah Data
-# Update Data
-# Delete Data
+        show_menu()
