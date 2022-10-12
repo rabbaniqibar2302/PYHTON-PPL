12dccc38dafe162bc6f8c0a85bc4d6afb4d82409
rizkiakbar2302@gmail.com <rizkiakbar2302@gmail.com>
2022-10-12 08:12:03 +0700

    update inheritance xi rpl 1

--- RPL 2/latihan5.py
+++ RPL 2/latihan5.py
@@ -1,6 +1,6 @@
-#Belajar Pewarisan (inheritance)
-#3 Objek = Orang, Pelajar, Pekerja
-#masing2 mempunyai nama, asal, kemampuan untuk introduce ur self
+# Belajar Pewarisan (inheritance)
+# 3 Objek = Orang, Pelajar, Pekerja
+# masing2 mempunyai nama, asal, kemampuan untuk introduce ur self
 # pelajar di sekolah , pekerja di kantor
 
 class Orang:
@@ -9,27 +9,30 @@ class Orang:
         self.asal = asal
 
     def perkenalan(self):
-        print("Perkenalkan nama saya", self.nama,"dari", self.asal)
+        print("Perkenalkan nama saya", self.nama, "dari", self.asal)
+
 
 class Pelajar(Orang):
-    #Sekolahnya dimana
+    # Sekolahnya dimana
     def __init__(self, nama, asal, sekolah):
         Orang.__init__(self, nama, asal)
         self.sekolah = sekolah
 
+
 class Pekerja(Orang):
-    #Kerjanya dimana
+    # Kerjanya dimana
     def __init__(self, nama, asal, kerja):
         Orang.__init__(self, nama, asal)
-        self.kerja = kerja    
+        self.kerja = kerja
 
-dani = Orang('Dani','Zimbabwe\n')
+
+dani = Orang('Dani', 'Zimbabwe\n')
 dani.perkenalan()
 
-rahma = Pelajar('rahma','jawa','SMK JP 1')
+rahma = Pelajar('rahma', 'jawa', 'SMK JP 1')
 rahma.perkenalan()
-print("Saya Bersekolah di", rahma.sekolah,"\n")
+print("Saya Bersekolah di", rahma.sekolah, "\n")
 
-arya = Pekerja('arya','pakistan','SMK JP 1')
+arya = Pekerja('arya', 'pakistan', 'SMK JP 1')
 arya.perkenalan()
-print("Saya Bekerja di", arya.kerja,"\n")
+print("Saya Bekerja di", arya.kerja, "\n")
